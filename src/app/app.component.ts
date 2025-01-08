f69dce236e0258f5f98847452a5b50abad7b50d8
Sergio Rodrigues <sergiohrodriguess@gmail.com>
2025-01-08 14:37:34 -0300

    meu primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MeuPrimeiroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'learning-angular';
+  title = 'Olá mundo!';
 }
